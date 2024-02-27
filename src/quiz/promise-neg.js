370389416b9da35be101238fd3c1a15908434a57
Brendan Baron <bbaron4194@gmail.com>
2024-02-27 17:00:56 -0500

    Changes from class

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,29 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function hasNegative(arr, idx) {
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(arr.length > idx) {
+            setTimeout(() => {
+                arr[idx].filter((n) => n < 0).length > 0 ? resolve(arr[idx]) : reject("no negative");
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: invalid idx');
+        }
+    });
+}
+
+const rowSumPromises = [];
+for (let x = 0; x < array2D.length; x++) {
+    rowSumPromises.push(hasNegative(array2D, x));
+}
+
+Promise.any(rowSumPromises)
+    .then((row) => {
+        console.log(row);
+    })
+    .catch((err) => console.log(err));
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,36 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+function sumRow(arr, idx) {
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(arr.length > idx) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr[idx].length; i++) {
+                    sum += arr[idx][i];
+                }
+                console.log('resolving ... ');
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: invalid idx');
+        }
+    });
+}
+
+const rowSumPromises = [];
+for (let x = 0; x < array2D.length; x++) {
+    rowSumPromises.push(sumRow(array2D, x));
+}
+
+Promise.all(rowSumPromises)
+    .then((rowSums) => {
+        let sum = 0;
+        rowSums.forEach((s) => sum += s);
+        console.log(sum);
+    })
+    .catch((err) => console.log(err));
